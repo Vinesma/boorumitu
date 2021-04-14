203c0607ab0791b84517bccbef26fdaa436bb5f2
Vinesma <otaviocos_13@hotmail.com>
2021-04-13 22:13:00 -0300

    Add Video component and loading state

--- components/Gallery/subComponents/ImageModal.tsx
+++ components/Gallery/subComponents/ImageModal.tsx
@@ -1,5 +1,19 @@
 import React from 'react';
-import { Modal, View, Image, StyleSheet, useWindowDimensions } from 'react-native';
+import {
+    Modal,
+    View,
+    Animated,
+    StyleSheet,
+    useWindowDimensions
+} from 'react-native';
+import {
+    PinchGestureHandler,
+    HandlerStateChangeEvent,
+    PinchGestureHandlerEventPayload,
+    State
+} from 'react-native-gesture-handler';
+import Video from 'react-native-video';
+import Loader from '../../Loader/Loader';
 
 interface Props {
     visible: boolean,
@@ -8,8 +22,23 @@ interface Props {
 }
 
 const ImageModal = ({ visible, uri, handleModal }: Props): JSX.Element => {
+    const [loading, setLoading] = React.useState<boolean>(true);
     const width = useWindowDimensions().width;
     const height = useWindowDimensions().height;
+    const scale = React.useRef(new Animated.Value(1)).current;
+
+    const onGestureEvent = Animated.event([
+        { nativeEvent: { scale } }
+    ], { useNativeDriver: true });
+
+    const onHandlerStateChange = (event: HandlerStateChangeEvent<PinchGestureHandlerEventPayload>) => {
+        if (event.nativeEvent.oldState === State.ACTIVE) {
+            Animated.spring(scale, {
+                toValue: 1,
+                useNativeDriver: true,
+            }).start();
+        }
+    }
 
     return (
         <Modal
@@ -17,16 +46,35 @@ const ImageModal = ({ visible, uri, handleModal }: Props): JSX.Element => {
             visible={visible}
             onRequestClose={() => handleModal()}
         >
-            <View style={styles.container}>
-                <Image
-                    resizeMode="center"
-                    style={{
-                        width,
-                        height,
-                    }}
+            { loading ? <Loader size="large" color="#ee9818"/> : null }
+            { uri.endsWith(".mp4") ? (
+                <Video
                     source={{ uri }}
+                    style={[{ width, height }, styles.player]}
+                    controls
+                    muted
+                    repeat
+                    resizeMode="contain"
+                    onLoadStart={() => setLoading(true)}
+                    onReadyForDisplay={() => setLoading(false)}
                 />
-            </View>
+            ) : (
+                <View style={styles.container}>
+                    <PinchGestureHandler onGestureEvent={() => onGestureEvent()} onHandlerStateChange={event => onHandlerStateChange(event)}>
+                        <Animated.Image
+                            resizeMode="center"
+                            onLoadStart={() => setLoading(true)}
+                            onLoadEnd={() => setLoading(false)}
+                            style={{
+                                width,
+                                height,
+                                transform: [{ scale }],
+                            }}
+                            source={{ uri }}
+                        />
+                    </PinchGestureHandler>
+                </View>
+            )}
         </Modal>
     )
 };
@@ -36,6 +84,9 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#242424',
     },
+    player: {
+        backgroundColor: '#242424',
+    }
 });
 
 export default ImageModal;
